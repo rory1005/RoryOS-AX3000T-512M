a0c06a180a9bd6709412b4b915bb820cf650d055
rory <ncll1986@gmail.com>
2026-07-01 04:44:11 +0800

    Update progress messages in customize.sh script

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -13,7 +13,7 @@ if [ ! -d "$OPENWRT_DIR" ]; then
   exit 1
 fi
 
-echo "[1/3] Set default hostname..."
+echo "[1/4] Set default hostname..."
 mkdir -p "$OPENWRT_DIR/files/etc/config"
 
 cat > "$OPENWRT_DIR/files/etc/config/system" <<'EOF'
@@ -26,7 +26,36 @@ config system
 	option urandom_seed '0'
 EOF
 
-echo "[2/3] Banner..."
+echo "[2/4] Set default LAN IP..."
+mkdir -p "$OPENWRT_DIR/files/etc/config"
+
+cat > "$OPENWRT_DIR/files/etc/config/network" <<'EOF'
+config interface 'loopback'
+	option device 'lo'
+	option proto 'static'
+	option ipaddr '127.0.0.1'
+	option netmask '255.0.0.0'
+
+config globals 'globals'
+	option ula_prefix 'auto'
+
+config device
+	option name 'br-lan'
+	option type 'bridge'
+	list ports 'lan1'
+	list ports 'lan2'
+	list ports 'lan3'
+	list ports 'wan'
+
+config interface 'lan'
+	option device 'br-lan'
+	option proto 'static'
+	option ipaddr '192.168.10.1'
+	option netmask '255.255.255.0'
+	option ip6assign '60'
+EOF
+
+echo "[3/4] Banner..."
 echo "Banner OK"
 
-echo "[3/3] RoryOS Customize Done!"
+echo "[4/4] RoryOS Customize Done!"
