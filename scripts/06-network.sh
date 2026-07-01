5e89f60cfeeddf3bf2e922998fbbe54e71565d69
rory <ncll1986@gmail.com>
2026-07-01 08:04:01 +0800

    Configure default LAN IP for RoryOS
    Added default network configuration for RoryOS, setting LAN IP and netmask.

--- scripts/06-network.sh
+++ scripts/06-network.sh
@@ -10,10 +10,8 @@
 #     网络配置模块。
 #
 # 当前功能：
-#     ① LAN 配置
-#     ② WAN 配置
-#     ③ DHCP
-#     ④ DNS
+#     ① 设置默认 LAN IP 为 192.168.10.1
+#     ② 保留 OpenWrt 默认网络结构，使用 uci-defaults 首次开机自动修改
 #
 # 作者：
 #     Rory
@@ -32,8 +30,25 @@ set -e
 
 OPENWRT_DIR="${1:-openwrt}"
 
-echo "【网络】开始..."
+echo "【网络】开始配置默认网络..."
 
-# 后续网络配置将在这里添加
+mkdir -p "$OPENWRT_DIR/files/etc/uci-defaults"
 
-echo "【网络】完成。"
+cat > "$OPENWRT_DIR/files/etc/uci-defaults/98-roryos-network" <<'EOF'
+#!/bin/sh
+
+# RoryOS 默认网络配置
+# 首次开机自动执行
+
+uci set network.lan.ipaddr='192.168.10.1'
+uci set network.lan.netmask='255.255.255.0'
+uci set network.lan.ip6assign='60'
+
+uci commit network
+
+exit 0
+EOF
+
+chmod +x "$OPENWRT_DIR/files/etc/uci-defaults/98-roryos-network"
+
+echo "【网络】默认网络配置完成。"
